99f8a0e3135d0dd1ca6f345704638ae260480fd9
Satesh <janki.chaudhari@isoftbet.com>
2020-05-27 23:54:08 +0100

    Commit1

--- src/test/java/regression/customerPortalPOM/InfoNewsContactPage.java
+++ src/test/java/regression/customerPortalPOM/InfoNewsContactPage.java
@@ -1,5 +1,27 @@
 package regression.customerPortalPOM;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class InfoNewsContactPage extends basePage {
+
+    @FindBy(how = How.XPATH,
+            using = "//div[contains(@class,'header-navbar bg-menu-bar')]//a[contains(@class,'dropdown-toggle')][contains(text(),'INFO PAGES')]")
+    WebElement infoPages;
+
+
+
+    public void infoPagesLink(){
+        driver.findElement(By.xpath("//div[@class='modal-body']//div[@class='container']//span[contains(text(),'×')]")).click();
+        infoPages.isDisplayed();
+    }
+
+
+
+
+
 
 }
